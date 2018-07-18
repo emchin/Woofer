3c74e15ccfc4fa588b6ed85041d0ede0f9fec12f
M <8013994+emchin@users.noreply.github.com>
2018-07-18 15:05:36 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function new_user() {
+          var user_database = {}
+          var name = document.getElementById('username').value;
+          var pass = document.getElementById('password').value;
+          user_database[name] = pass;
+      }
+document.getElementById('submit').addEventListener('click', say_hi);
+
+function check_user() {
+          var maybe_user = document.getElementById('user-user').value;
+          var maybe_pass = document.getElementById('user-pass').value;
+          if (user_database[maybe_user] == maybe_pass){
+              document.getElementById("test").innerHTML = "You logged in.";
+          }
+      }

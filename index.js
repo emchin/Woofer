87467f99d25e84984d13596e71180ae5b91e8b39
M <8013994+emchin@users.noreply.github.com>
2018-07-19 14:10:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function new_user() {
-          var user_database = {}
-          var name = document.getElementById('username').value;
-          var pass = document.getElementById('password').value;
-          user_database[name] = pass;
-          document.getElementById("test").innerHTML = "hello";
-      }
+function check_user() {
+          var maybe_user = document.getElementById('user-user').value;
+	var maybe_pass = document.getElementById('user-pass').value;
+	var user_database = global.localStorage.getItem("user_database");
+	if(user_database[maybe_user] == maybe_pass){
+		document.getElementById("test").innerHTML = "You logged in.";
+		window.open("introAcct.html");
+	} else {
+		document.getElementById("test").innerHTML = "Sorry, that's the wrong username/password. Please try again!";
+		window.alert("Username/Password failed! Please try again.");
+	}
+}
+        	document.getElementById('submit').addEventListener('click', check_user);
 
 var user_database = {}
 function new_user() {

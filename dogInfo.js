7a52bb0866aed1849ac815b8c991ed116e272dcd
JenniferZwiebel <41163987+JenniferZwiebel@users.noreply.github.com>
2018-07-19 14:23:40 -0700

    Update dogInfo.js

--- dogInfo.js
+++ dogInfo.js
@@ -1,1 +1,29 @@
 
+ <script>
+        function new_dog() {
+          var dogName = document.getElementsByName("dogName").value;
+          
+          var zipcode = document.getElementsByName("zipcode").value;
+          var ownerName = document.getElementsByName("ownerName").value;
+          var contactInfo = document.getElementsByName("contactInfo").value;
+           
+          localStorage.setItem(zipcode, ownerName);
+          var color1 = document.getElementsByName("color1").checked;
+          var color2 = document.getElementsByName("color2").checked;
+          var color3 = document.getElementsByName("color3").checked;
+          var color4 = document.getElementsByName("color4").checked;
+          var color5 = document.getElementsByName("color5").checked;
+          var color6 = document.getElementsByName("color6").checked;
+          var color7 = document.getElementsByName("color7").checked;
+          var eyecolor = document.getElementsByName("dog-eye-colors").value;
+          var pattern = document.getElementsByName("dog-pattern").value;
+          var size = document.getElementsByName("dog-size").value;
+          var image = document.getElementById("cameraInput");
+
+
+          dogName = [zipcode, ownerName, contactInfo, color1, color2, color3, color4, color5, color6, color7, eyecolor, pattern, size, image];
+          window.open("dogProfile.html");
+      }
+        document.getElementById("submit").addEventListener('click', new_dog);
+
+      </script>

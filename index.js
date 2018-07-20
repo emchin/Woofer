c1e1e884024b9819d673ef53abed91a0caf1b685
M <8013994+emchin@users.noreply.github.com>
2018-07-20 10:51:32 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+      apiKey: "AIzaSyD_iE72gO_qiX1KMH1kJGT7d-hzFscjS1w",
+      authDomain: "woofer-3c420.firebaseapp.com",
+      databaseURL: "https://woofer-3c420.firebaseio.com",
+      projectId: "woofer-3c420",
+      storageBucket: "",
+      messagingSenderId: "723483471961"
+};
+firebase.initializeApp(config);

5a2d709ebe337a61cfbc5b898633ca93fa6f2883
M <8013994+emchin@users.noreply.github.com>
2018-07-20 12:22:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,16 +32,19 @@
             const pass = txt_pass;
             const auth = firebase.auth();
             //Sign up!
-            auth.createUserWithEmailAndPassword(email, pass);
-      });
-      
-      //Add a realtime listener
-      firebase.auth().onAuthStateChanged(firebaseUser => {
-            if(firebaseUser) {
-                  console.log(firebaseUser);
-            } else {
-                  console.log('Not logged in.');
-            }
+            firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+                  // Handle Errors here.
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  // [START_EXCLUDE]
+                  if (errorCode == 'auth/weak-password') {
+                        alert('The password is too weak.');
+                  } else {
+                        alert(errorMessage);
+                  }
+                  console.log(error);
+             // [END_EXCLUDE]
+            });
       });
       
 }());     

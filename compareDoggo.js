09be09658b9f4bdb1378c2f6d4db0c8572a22127
M <8013994+emchin@users.noreply.github.com>
2018-07-23 14:07:45 -0700

    Update compareDoggo.js

--- compareDoggo.js
+++ compareDoggo.js
@@ -1,4 +1,4 @@
-alert("Woof!")
+alert("Woof!");
 
 var dogName = document.getElementsByName("dogName").value;
 var zipcode = document.getElementsByName("zipcode").value;
@@ -14,7 +14,8 @@ var color7 = document.getElementsByName("color7").checked;
 var eyecolor = document.getElementsByName("dog-eye-colors").value;
 var pattern = document.getElementsByName("dog-pattern").value;
 var size = document.getElementsByName("dog-size").value;
-var image = document.getElementById("cameraInput");
 
+var img = document.getElementById("output_image");
+var image = img.src;
 
 dogName = [zipcode, ownerName, contactInfo, color1, color2, color3, color4, color5, color6, color7, eyecolor, pattern, size, image];

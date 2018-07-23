04be9f589a9e3f27afc3dc6b12bd0a7f98c5b6f4
M <8013994+emchin@users.noreply.github.com>
2018-07-23 10:16:34 -0700

    Update findDog.js

--- findDog.js
+++ findDog.js
@@ -1,3 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD_iE72gO_qiX1KMH1kJGT7d-hzFscjS1w",
+    authDomain: "woofer-3c420.firebaseapp.com",
+    databaseURL: "https://woofer-3c420.firebaseio.com",
+    projectId: "woofer-3c420",
+    storageBucket: "woofer-3c420.appspot.com",
+    messagingSenderId: "723483471961"
+  };
+  firebase.initializeApp(config);
+
 // Get a reference to the database service
 var database = firebase.database();
 

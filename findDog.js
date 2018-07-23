bb6a62280d3ff33b3263061b6b3ad170c148ce04
M <8013994+emchin@users.noreply.github.com>
2018-07-23 10:05:19 -0700

    Added semicolons

--- findDog.js
+++ findDog.js
@@ -4,7 +4,7 @@ var database = firebase.database();
 //Function to save information of found dog
 function saveFoundDog(zipcode_r, owner_name_r, dog_name_r, owner_phone_r, owner_email_r, coat_color_r, size_r, eye_color_r, coat_pattern_r) {
   firebase.database.ref('\found-dog').set ({
-    zipcode_r = document.getElementById("zipcode")
-    owner_name_r = document.getElementById("owner_name")
-    dog_name_r = document.getElementById("dog_name")
+    zipcode_r : document.getElementById("zipcode");
+    owner_name_r : document.getElementById("owner_name");
+    dog_name_r : document.getElementById("dog_name");
 }

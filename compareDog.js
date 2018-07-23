5cfc9127f38c6295eb3d2132d52a9b6afb6bfe6f
M <8013994+emchin@users.noreply.github.com>
2018-07-23 12:31:23 -0700

    Update compareDog.js

--- compareDog.js
+++ compareDog.js
@@ -1,1 +1,8 @@
-alert("Ruff ruff!")
+alert("Ruff ruff!");
+new_dog = "Snoopy";
+new_dog = {
+  var owner = "Charlie Brown";
+  var owner_phone = "123-456-7890";
+}
+f = open("doggos.txt", "a")
+f.write(new_dog)

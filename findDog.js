2723f3f7a93e36917e2b28cd68e1141d674b1213
M <8013994+emchin@users.noreply.github.com>
2018-07-23 10:03:55 -0700

    Update findDog.js

--- findDog.js
+++ findDog.js
@@ -2,6 +2,9 @@
 var database = firebase.database();
 
 //Function to save information of found dog
-function saveFoundDog(zipcode, owner_name, dog_name, owner_phone, owner_email, coat_color, size, eye_color, coat_pattern) {
-
+function saveFoundDog(zipcode_r, owner_name_r, dog_name_r, owner_phone_r, owner_email_r, coat_color_r, size_r, eye_color_r, coat_pattern_r) {
+  firebase.database.ref('\found-dog').set ({
+    zipcode_r = document.getElementById("zipcode")
+    owner_name_r = document.getElementById("owner_name")
+    dog_name_r = document.getElementById("dog_name")
 }

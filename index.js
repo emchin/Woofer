e1c5731e1199489f12f5d589541b1f87508c5d1d
nivrithikrishnan <41163965+nivrithikrishnan@users.noreply.github.com>
2018-07-23 10:02:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -47,12 +47,6 @@
             });
       });
       
-}());     
-<script>
-      // When the user clicks on <div>, open the popup
-      function myFunction() {
-          var popup = document.getElementById("myPopup");
-          popup.classList.toggle("show");
-      }
-</script>
+}());  
+
 

06a08800c57659a399f846afe7738e042e5ee077
M <8013994+emchin@users.noreply.github.com>
2018-07-23 09:49:55 -0700

    Create findDog.js

---
+++ findDog.js
@@ -0,0 +1,7 @@
+// Get a reference to the database service
+var database = firebase.database();
+
+//Function to save information of found dog
+function saveFoundDog(zipcode, owner_name, dog_name, owner_phone, owner_email, coat_color, size, eye_color, coat_pattern) {
+
+}

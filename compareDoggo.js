8848535b5f1c32975a62dd145321c1c3d7e3ef35
M <8013994+emchin@users.noreply.github.com>
2018-07-23 20:38:04 -0700

    Added message

--- compareDoggo.js
+++ compareDoggo.js
@@ -2,7 +2,8 @@ function set_dog() {
   var randomNumber = 0;
   while (localStorage.getItem(randomNumber) != null) {
     randomNumber += 1;
-
+  
+  alert(randomNumber);
   var dogName = document.getElementsByName("dogName").value;
   var zipcode = document.getElementsByName("zipcode").value;
   var ownerName = document.getElementsByName("ownerName").value;
@@ -23,5 +24,9 @@ function set_dog() {
   dogName = [document.getElementsByName("dogName").value, zipcode, ownerName, contactInfo, color1, color2, color3, color4, color5, color6, color7, eyecolor, pattern, size, image];
   
   localStorage.setItem(randomNumber, dogName);
-  alert(randomNumber);
   }
+
+function retreive_dog(dogName[0]) {
+  var message = '<h2> You added <h1>' + dogName[0] + '</h1> to our database!</h2> <br> <p> Well done! Now if </p>' + dogName[0] ' happens to wander (or sneak) away, anyone with Internet access can use Woofer to get them home!';
+  document.getElementById('congrats').innerHTML = message
+}
